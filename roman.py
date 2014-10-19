e5f54835de1776f345467c968890f593d8a45d81
sqlwwx <wwx_2012@hotmail.com>
2014-10-19 19:56:50 +0800

    使用查询表重构代码

--- roman.py
+++ roman.py
@@ -7,7 +7,6 @@ This program is part of 'Dive Into Python 3', a free Python book for
 experienced programmers.  Visit http://diveintopython3.org/ for the
 latest version.
 '''
-import re
 
 class OutOfRangeError(ValueError): pass
 class NotIntegerError(ValueError): pass
@@ -27,31 +26,16 @@ roman_numeral_map = (('M',  1000),
                      ('IV', 4),
                      ('I',  1))
 
-roman_numeral_pattern = re.compile('''
-    ^                   # beginning of string
-    M{0,4}              # thousands - 0 to 4 M's
-    (CM|CD|D?C{0,3})    # hundreds - 900 (CM), 400 (CD), 0-300 (0 to 3 C's),
-                        #            or 500-800 (D, followed by 0 to 3 C's)
-    (XC|XL|L?X{0,3})    # tens - 90 (XC), 40 (XL), 0-30 (0 to 3 X's),
-                        #        or 50-80 (L, followed by 0 to 3 X's)
-    (IX|IV|V?I{0,3})    # ones - 9 (IX), 4 (IV), 0-3 (0 to 3 I's),
-                        #        or 5-8 (V, followed by 0 to 3 I's)
-    $                   # end of string
-    ''', re.VERBOSE)
+to_roman_table = [ None ]
+from_roman_table = {}
 
 def to_roman(n):
     '''整数转化为罗马数字'''
+    if int(n) != n:
+        raise NotIntegerError('无法转化非整数')
     if not (0 < n < 5000):
         raise OutOfRangeError('n 必须是 1...3999')
-    if not isinstance(n, int):
-        raise NotIntegerError('无法转化非整数')
-
-    result = ''
-    for numeral, integer in roman_numeral_map:
-        while n >= integer:
-            result += numeral
-            n -= integer
-    return result
+    return to_roman_table[n]
 
 def from_roman(s):
     '''罗马数字转化为整数'''
@@ -59,17 +43,28 @@ def from_roman(s):
         raise InvalidRomanNumeralError('Input must be a string')
     if not s:
         raise InvalidRomanNumeralError('Input can not be blank')
-    if not roman_numeral_pattern.search(s):
-        raise InvalidRomanNumeralError('Invalid Roman numeral: {}'.format(s))
+    if s not in from_roman_table:
+        raise InvalidRomanNumeralError('Invalid Roman numeral: {0}'.format(s))
+    return from_roman_table[s]
 
-    result = 0
-    index = 0
-    for numeral, integer in roman_numeral_map:
-        while s[index : index + len(numeral)] == numeral:
-            result += integer
-            index += len(numeral)
-    return result
+def build_lookup_tables():
+    def to_roman(n):
+        result = ''
+        for numeral, integer in roman_numeral_map:
+            if n >= integer:
+                result = numeral
+                n -= integer
+                break
+        if n > 0:
+            result += to_roman_table[n]
+        return result
 
+    for integer in range(1, 5000):
+        roman_numeral = to_roman(integer)
+        to_roman_table.append(roman_numeral)
+        from_roman_table[roman_numeral] = integer
+
+build_lookup_tables()
 # Copyright (c) 2009, Mark Pilgrim, All rights reserved.
 # 
 # Redistribution and use in source and binary forms, with or without modification,
