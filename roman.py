65332b18aa0d593cc5ae4ad7f002ce422762a3d5
sqlwwx <wwx_2012@hotmail.com>
2014-10-19 19:29:46 +0800

    实现fromRoman输入检查

--- roman.py
+++ roman.py
@@ -7,6 +7,7 @@ This program is part of 'Dive Into Python 3', a free Python book for
 experienced programmers.  Visit http://diveintopython3.org/ for the
 latest version.
 '''
+import re
 
 class OutOfRangeError(ValueError): pass
 class NotIntegerError(ValueError): pass
@@ -26,6 +27,18 @@ roman_numeral_map = (('M',  1000),
                      ('IV', 4),
                      ('I',  1))
 
+roman_numeral_pattern = re.compile('''
+    ^                   # beginning of string
+    M{0,3}              # thousands - 0 to 3 M's
+    (CM|CD|D?C{0,3})    # hundreds - 900 (CM), 400 (CD), 0-300 (0 to 3 C's),
+                        #            or 500-800 (D, followed by 0 to 3 C's)
+    (XC|XL|L?X{0,3})    # tens - 90 (XC), 40 (XL), 0-30 (0 to 3 X's),
+                        #        or 50-80 (L, followed by 0 to 3 X's)
+    (IX|IV|V?I{0,3})    # ones - 9 (IX), 4 (IV), 0-3 (0 to 3 I's),
+                        #        or 5-8 (V, followed by 0 to 3 I's)
+    $                   # end of string
+    ''', re.VERBOSE)
+
 def to_roman(n):
     '''整数转化为罗马数字'''
     if not (0 < n < 4000):
@@ -42,6 +55,9 @@ def to_roman(n):
 
 def from_roman(s):
     '''罗马数字转化为整数'''
+    if not roman_numeral_pattern.search(s):
+        raise InvalidRomanNumeralError('Invalid Roman numeral: {}'.format(s))
+
     result = 0
     index = 0
     for numeral, integer in roman_numeral_map:
